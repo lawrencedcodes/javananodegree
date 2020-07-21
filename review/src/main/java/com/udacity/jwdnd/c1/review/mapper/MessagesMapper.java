9e2b2eec30ec09e360776eb2e26effe9fbe1402b
Lawrence D. Lockhart, Jr <ldlockhart@gmail.com>
2020-07-21 02:21:39 -0500

    Slap some bootstrap on that bad boy

--- review/src/main/java/com/udacity/jwdnd/c1/review/mapper/MessagesMapper.java
+++ review/src/main/java/com/udacity/jwdnd/c1/review/mapper/MessagesMapper.java
@@ -6,13 +6,13 @@ import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Select;
 
-public interface UserMapper {
-    @Select("SELECT * FROM USERS WHERE username = #{username} ")
+public interface MessagesMapper {
+    @Select("SELECT * FROM MESSAGES WHERE username = #{username} ")
     User getUser(String username);
 
-    @Insert("INSERT INTO USERS (username, salt, password, firstname, lastname)" + "VALUES(#{username}, #{salt}, #{password}, #{firstname},#{lastname})")
-    @Options(useGeneratedKeys = true, keyProperty = "userId")
-     int insert(User user);
+    @Insert("INSERT INTO MESSAGES (username, messagetext)" + "VALUES(#{username}, #{messagetext})")
+    @Options(useGeneratedKeys = true, keyProperty = "messageId")
+    int insert(User user);
 
     @Delete("DELETE FROM USERS WHERE username = #{username} ")
     void delete(String username);
