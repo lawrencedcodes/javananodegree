8bdb920ea24feccd7ef6afaa0d9a402899d536fd
Lawrence D. Lockhart, Jr <ldlockhart@gmail.com>
2020-07-21 01:11:43 -0500

    add skeleton final review lesson5

---
+++ review/src/main/java/com/udacity/jwdnd/c1/review/mapper/UserMapper.java
@@ -0,0 +1,4 @@
+package com.udacity.jwdnd.c1.review.mapper;
+
+public class UserMapper {
+}
---
+++ review/src/main/java/com/udacity/jwdnd/c1/review/model/User.java
@@ -0,0 +1,10 @@
+package com.udacity.jwdnd.c1.review.model;
+
+public class User {
+    private int userid;
+    private String username;
+    private String salt;
+    private String password;
+    private String firstname;
+    private String lastname;
+}
