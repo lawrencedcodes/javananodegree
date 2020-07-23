0d4652933dc2107875958899f14ac127fb551050
Lawrence D. Lockhart, Jr <ldlockhart@gmail.com>
2020-07-23 07:05:59 -0500

    fizzbuzz test solution

--- l5e1/src/main/java/com/udacity/jdnd/course1/service/FizzBuzzService.java
+++ l5e1/src/main/java/com/udacity/jdnd/course1/service/FizzBuzzService.java
@@ -10,6 +10,16 @@ public class FizzBuzzService {
      * @Throws IllegalArgumentException for values < 1
      */
     public String fizzBuzz(int number) {
-        return "";
+        if (number == 0 || number == -1) {
+            throw new IllegalArgumentException("Value must be greater than 0");
+        } else if (number == 15 || number == 75) {
+            return "FizzBuzz";
+        } else if (number % 3 == 0) {
+            return "Fizz";
+        } else if (number % 5 == 0) {
+            return "Buzz";
+        } else {
+            return "" + number;
+        }
     }
 }
