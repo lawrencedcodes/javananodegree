94e781356835fff2cbf6a12a8990f41c387e397c
Lawrence Lockhart <ldlockhart@gmail.com>
2020-11-28 12:01:11 -0600

    add pojo to contain data model object

--- nd035-c1-spring-boot-basics-examples-master/udacity-jwdnd-c1-l3-mvc-and-you-1-master/src/main/java/com/example/mvc_basics/controller/HomeController.java
+++ nd035-c1-spring-boot-basics-examples-master/udacity-jwdnd-c1-l3-mvc-and-you-1-master/src/main/java/com/example/mvc_basics/controller/HomeController.java
@@ -4,13 +4,15 @@ import java.time.Instant;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
-    @RequestMapping("/home")
+    @GetMapping("/home")
     public String getHomePage(Model model) {
+        model.addAttribute("newMessage", object);
         model.addAttribute("welcomeMessage", Instant.now().toString());
         model.addAttribute("messages",
                 new String[] { "First Greeting", "Second Greeting", "Third Greeting", "Fourth Greeting" });
