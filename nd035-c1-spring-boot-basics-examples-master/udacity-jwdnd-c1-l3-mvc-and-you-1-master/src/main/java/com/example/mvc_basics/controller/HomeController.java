e3b0d1b5c8846025b98bcd5ac86bf452109e4eb2
Lawrence Lockhart <ldlockhart@gmail.com>
2020-11-27 22:26:59 -0600

    add minor thymeleaf snippets

--- nd035-c1-spring-boot-basics-examples-master/udacity-jwdnd-c1-l3-mvc-and-you-1-master/src/main/java/com/example/mvc_basics/controller/HomeController.java
+++ nd035-c1-spring-boot-basics-examples-master/udacity-jwdnd-c1-l3-mvc-and-you-1-master/src/main/java/com/example/mvc_basics/controller/HomeController.java
@@ -1,13 +1,19 @@
 package com.example.mvc_basics.controller;
 
+import java.time.Instant;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
     @RequestMapping("/home")
-    public String getHomePage() {
+    public String getHomePage(Model model) {
+        model.addAttribute("welcomeMessage", Instant.now().toString());
+        model.addAttribute("messages",
+                new String[] { "First Greeting", "Second Greeting", "Third Greeting", "Fourth Greeting" });
         return "home";
     }
 
