ff03b47bce2c40251dae0a50a55676e882f56f61
Lawrence Lockhart <ldlockhart@gmail.com>
2020-12-13 02:54:39 -0600

    Add user object

--- nd035-c1-spring-boot-basics-examples-master/udacity-jwdnd-c1-l3-mvc-and-you-1-master/src/main/java/com/example/mvc_basics/model/User.java
+++ nd035-c1-spring-boot-basics-examples-master/udacity-jwdnd-c1-l3-mvc-and-you-1-master/src/main/java/com/example/mvc_basics/model/User.java
@@ -1,5 +1,104 @@
 package com.example.mvc_basics.model;
 
 public class User {
+    private Integer userid;
+    private String username;
+    private String salt;
+    private String password;
+    private String firstname;
+    private String lastname;
+
+    public User(Integer userid, String username, String salt, String password, String firstname, String lastname) {
+        this.userid = userid;
+        this.username = username;
+        this.salt = salt;
+        this.password = password;
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+
+    /**
+     * @return Integer return the userid
+     */
+    public Integer getUserid() {
+        return userid;
+    }
+
+    /**
+     * @param userid the userid to set
+     */
+    public void setUserid(Integer userid) {
+        this.userid = userid;
+    }
+
+    /**
+     * @return String return the username
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * @param username the username to set
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    /**
+     * @return String return the salt
+     */
+    public String getSalt() {
+        return salt;
+    }
+
+    /**
+     * @param salt the salt to set
+     */
+    public void setSalt(String salt) {
+        this.salt = salt;
+    }
+
+    /**
+     * @return String return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * @return String return the firstname
+     */
+    public String getFirstname() {
+        return firstname;
+    }
+
+    /**
+     * @param firstname the firstname to set
+     */
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    /**
+     * @return String return the lastname
+     */
+    public String getLastname() {
+        return lastname;
+    }
+
+    /**
+     * @param lastname the lastname to set
+     */
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
 
 }
