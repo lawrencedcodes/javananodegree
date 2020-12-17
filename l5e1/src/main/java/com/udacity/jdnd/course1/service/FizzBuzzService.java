7e47e450aa2dfdf8fd7ced39e9a167350661a1cc
Lawrence Lockhart <ldlockhart@gmail.com>
2020-12-17 16:10:18 -0600

    add junit test situ starter files

--- l5e1/src/main/java/com/udacity/jdnd/course1/service/FizzBuzzService.java
+++ l5e1/src/main/java/com/udacity/jdnd/course1/service/FizzBuzzService.java
@@ -3,9 +3,9 @@ package com.udacity.jdnd.course1.service;
 public class FizzBuzzService {
 
     /**
-     * If number is divisible by 3, return "Fizz". If divisible by 5,
-     * return "Buzz", and if divisible by both, return "FizzBuzz". Otherwise,
-     * return the number itself.
+     * If number is divisible by 3, return "Fizz". If divisible by 5, return "Buzz",
+     * and if divisible by both, return "FizzBuzz". Otherwise, return the number
+     * itself.
      *
      * @Throws IllegalArgumentException for values < 1
      */
