619e8cc843e4e187db13a1cdfd94dd8997af2573
Lawrence Lockhart <ldlockhart@gmail.com>
2020-12-27 05:42:08 -0600

    Redo junit test material

---
+++ nd035-c1-spring-boot-basics-examples-master/kotlin-project/src/main/kotlin/Main.kt
@@ -0,0 +1,34 @@
+
+fun main() {
+    println("Hello world!")
+    val wholeNumber = 5
+    val firstFloat = 33.33f
+    val myString = "Lawrence"
+    val myDouble = 3.14
+    val firstSum = wholeNumber + myDouble
+    val loggedIn = false
+    val firstInitial = 'l'
+    val weight = 33.992f
+    val chemWeight = 48L
+    val firstName = "Lawrence"
+    firstName.
+
+    val i = 3
+    val j = 4
+    val k = 3*4
+    val diviFirst = 3f/4f
+
+    println(diviFirst)
+    println("The value of k is $k")
+    println("The sum is $firstSum.")
+    println(chemWeight)
+
+    val isLegal = true;
+    val isSingle = false;
+    val legalAndSingle = isLegal && isSingle
+    val legalOrSingle = isLegal || isSingle
+
+    println("We have determined your married and single status is $legalAndSingle.")
+    println("We have determined your married OR single status is $legalOrSingle")
+
+}
