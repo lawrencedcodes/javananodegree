22cd577894dc097b82646578c93f82f7a9746448
Lawrence Lockhart <ldlockhart@gmail.com>
2020-12-29 04:39:34 -0600

    level set

--- l5e3/src/test/java/com/example/demo/SeleniumTest.java
+++ l5e3/src/test/java/com/example/demo/SeleniumTest.java
@@ -2,6 +2,35 @@ package com.example.demo;
 
 public class SeleniumTest {
     public static void main(String[] args) throws InterruptedException {
+                //start the driver, open chrome to our target url
+                WebDriverManager.chromedriver().setup();
+                WebDriver driver = new ChromeDriver();
+                driver.get("http://localhost:8080/animal");
         
-    }
+                //find the fields we want by id and fill them in
+                WebElement inputField = driver.findElement(By.id("animalText"));
+                inputField.sendKeys("Manatee");
+        
+                inputField = driver.findElement(By.id("adjective"));
+                inputField.sendKeys("Whirling");
+        
+                List<WebElement> trainingResults = driver.findElements(By.className("trainingMessage"));
+        
+                // The field-values don’t clear on submit 
+                for(int i = 0; i < 5; i++) {
+                    
+                    inputField = driver.findElement(By.id("adjective"));
+                    inputField.submit();
+        
+                    System.out.println("trainingResults.size() = " + trainingResults.size());
+                }
+        
+                // then get the element by the class conclusionMessage and print it
+                WebElement conclusionResult = driver.findElement(By.className("conclusionMessage"));
+                System.out.println("conclusionResult.getText() = " + conclusionResult.getText());
+        
+                Thread.sleep(5000);
+                driver.quit();
+            }
+        }
 }
